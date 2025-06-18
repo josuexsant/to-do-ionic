500b992997b0ac96a2a1f3b29c1928abf108f6e9
Josue Santamaria <josuexsanta@gmail.com>
2025-06-18 13:56:05 -0600

    aqui funciona todo

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+export interface Message {
+  fromName: string;
+  subject: string;
+  date: string;
+  id: number;
+  read: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  public messages: Message[] = [
+    {
+      fromName: 'Tarea 1',
+      subject: 'Proximamente habra contenido aqui modificable ggg',
+      date: '9:32 AM',
+      id: 0,
+      read: false
+    },
+  ];
+
+  constructor() { }
+
+  public getMessages(): Message[] {
+    return this.messages;
+  }
+
+  public getMessageById(id: number): Message {
+    return this.messages[id];
+  }
+}
